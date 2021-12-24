cfb34a8e3f6931bf689c232a0bb164314755c8f7
Vikram Singh <vikramuvs@gmail.com>
2021-12-24 11:50:05 +0530

    eight

--- components/LatestEvents.js
+++ components/LatestEvents.js
@@ -1,4 +1,35 @@
 function LatestEvents({ data }) {
+  function renderMonth(month) {
+    //console.log(month);
+    switch (month) {
+      case "01":
+        return "Jan";
+      case "02":
+        return "Feb";
+      case "03":
+        return "Mar";
+      case "04":
+        return "Apr";
+      case "05":
+        return "May";
+      case "06":
+        return "Jun";
+      case "07":
+        return "Jul";
+      case "08":
+        return "Aug";
+      case "09":
+        return "Sep";
+      case "10":
+        return "Oct";
+      case "11":
+        return "Nov";
+      case "12":
+        return "Dec";
+      default:
+        return null;
+    }
+  }
   return (
     <>
       {/* Dept Nav Bar */}
