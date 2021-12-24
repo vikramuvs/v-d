4e076719a07948ac82cb098bcd2fc76ccf112c85
Vikram Singh <vikramuvs@gmail.com>
2021-12-24 12:49:11 +0530

    16

--- components/DeptStats.js
+++ components/DeptStats.js
@@ -5,7 +5,7 @@ function DeptStats({ stats }) {
         {/* <div className="col-start-2 col-end-12 lg:col-start-4 lg:col-end-10 h-24 bg-gradient-to-r to-orange-ramaiah from-pink-ramaiah rounded-br-2xl rounded-bl-2xl shadow-md inline-flex justify-between lg:justify-evenly space-x-2 text-white"> */}
         <div className="hidden lg:inline-flex lg:col-start-4 lg:col-end-10 h-24 bg-gradient-to-r to-orange-ramaiah from-pink-ramaiah rounded-br-2xl rounded-bl-2xl shadow-md justify-between lg:justify-evenly space-x-2 text-white">
           <div className="flex items-center space-x-6">
-            {stats.map((stat, i) => {
+            {stats?.map((stat, i) => {
               return (
                 <div className="">
                   <div className="font-heading text-center text-3xl">
--- pages/dept/ch/faculty/[facultyId].js
+++ pages/dept/ch/faculty/[facultyId].js
@@ -37,7 +37,7 @@ export async function getStaticPaths() {
   //   ];
 
   // Get the paths we want to pre-render based on posts
-  const paths = await posts.map((faculty) => ({
+  const paths = posts.map((faculty) => ({
     params: { facultyId: faculty.id.toString() },
   }));
 
